fd10ad373d4938b52fe013a4b1d04b4d43b5eca5
nidajumail <168053145+nidajumail@users.noreply.github.com>
2025-05-15 08:42:20 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -334,8 +334,8 @@ if load_library():
               st.markdown("<div class='warning-message'>Your library is empty. Please add books to your library.</div>", unsafe_allow_html=True)
          else:
             cols = st.columns(2)
-            for i , book in enumerate(st.session_state.library):
-                 with cols[i % 2]:
+            for book in st.session_state.library:
+            
                       st.markdown(f"""<div class='book-card'>
                                   <h3>{book['title']}</h3>
                                   <p><strong>Author:</strong> {book['author']}</p>
